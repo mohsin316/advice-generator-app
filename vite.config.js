6d74d6f3f1a470c15bf6a60c4ef454b0367abb7c
Mohsin Miyaji <mohsin.kmiyaji@gmail.com>
2023-08-13 18:36:07 +0530

    fourth

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/advice-generator-app",
-  build: {
-    outDir: "build",
-  },
+  plugins: [react()],
 });
